edb3834d9bc3e4a5aef26029f9da12e57cdb2740
franalmeida@gmail.com <46890781+franalma@users.noreply.github.com>
2020-09-10 08:48:19 +0200

    First commit

---
+++ src/main/java/com/diusframi/android/stats/BatteryStats.java
@@ -0,0 +1,72 @@
+package com.diusframi.android.stats;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+
+public class BatteryStats {
+    private static BatteryStats instance;
+    private Intent batteryStatus;
+    private BatteryReceiver batteryReceiver;
+    private Log Log = com.diusframi.android.stats.Log.getInstance();
+
+    private BatteryStats(){}
+
+    public static BatteryStats getInstance(){
+        if (instance == null){
+            instance = new BatteryStats();
+        }
+        return instance; 
+    }
+
+    public void init(Context context){
+        if (context != null){
+            IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            filter.addAction(Intent.ACTION_POWER_CONNECTED);
+            filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+            filter.addAction(Intent.ACTION_BATTERY_LOW);
+            filter.addAction(Intent.ACTION_BATTERY_OKAY);
+            if (batteryReceiver != null){
+                context.unregisterReceiver(batteryReceiver);
+            }
+            batteryReceiver = new BatteryReceiver();
+            batteryStatus = context.registerReceiver(batteryReceiver, filter);
+
+        }
+    }
+
+
+    class BatteryReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null){
+                int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int batteryScale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                int batteryCurrentStatus = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+                boolean isCharging = batteryCurrentStatus == BatteryManager.BATTERY_STATUS_CHARGING
+                        || batteryCurrentStatus == BatteryManager.BATTERY_STATUS_FULL;
+                Log.d("batteryIsCharging: "+isCharging);
+                if(isCharging){
+                    int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+                    boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+                    boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+                    Log.d("batteryIsCharging with ac: "+ acCharge + " usb: "+usbCharge);
+                }
+                float batteryPct = batteryLevel * 100 / (float)batteryScale;
+                Log.d("batteryLevel: "+batteryPct);
+                int batteryHealth = batteryStatus.getIntExtra(BatteryManager.EXTRA_HEALTH, 0);
+                Log.d("batteryHealth : "+batteryHealth);
+
+
+
+            }
+
+
+        }
+    }
+
+
+}
---
+++ src/main/java/com/diusframi/android/stats/WiifStats.java
@@ -0,0 +1,89 @@
+package com.diusframi.android.stats;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+public class WiifStats {
+    private static WiifStats instance;
+    private NetworkConnectionChangeReceiver networkReceiver;
+    private NetworkInfo info;
+    private WiifStats(){}
+    private Log Log = com.diusframi.android.stats.Log.getInstance();
+    
+    public static WiifStats getInstance(){
+        if (instance == null){
+            instance = new WiifStats();
+        }
+        return instance; 
+    }
+
+    public void init(Context context){
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        info = cm.getActiveNetworkInfo();
+        boolean isConnected = info != null && info.isConnectedOrConnecting();
+        Log.d("isConnectedToWifi: "+ isConnected);
+
+        if (networkReceiver != null){
+            context.unregisterReceiver(networkReceiver);
+        }
+        networkReceiver = new NetworkConnectionChangeReceiver();
+        IntentFilter wifiStatus = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        context.registerReceiver(networkReceiver, wifiStatus);
+
+    }
+
+    public int getWifiSignalStrength(Context context){
+        if (context != null){
+            WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            int numberOfLevels = 5;
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int level = WifiManager.calculateSignalLevel(wifiInfo.getRssi(), numberOfLevels);
+            return  level;
+        }
+        return -1;
+    }
+
+    public int getWifiIp(Context context){
+        if (context != null){
+            WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            int numberOfLevels = 5;
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int ip = wifiInfo.getIpAddress();
+            return ip;
+        }
+        return -1;
+    }
+
+    public String getWifiSsid(Context context){
+        if (context != null){
+            WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService (Context.WIFI_SERVICE);
+            WifiInfo info = wifiManager.getConnectionInfo ();
+            String ssid  = info.getSSID();
+            return ssid;
+        }
+        return null;
+    }
+
+
+    class NetworkConnectionChangeReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean wifi = info.getType() == ConnectivityManager.TYPE_WIFI;
+            boolean mobile = info.getType() == ConnectivityManager.TYPE_MOBILE;
+            Log.d("connected to wifi: "+ wifi);
+            Log.d("connected to mobile: "+ mobile);
+            if (wifi) {
+                Log.d("wifi strength: "+getWifiSignalStrength(context));
+                Log.d("wifi name: "+getWifiSsid(context));
+                Log.d("wifi ip: "+getWifiIp(context));
+            }
+        }
+    }
+}
