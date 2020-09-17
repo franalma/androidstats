bbabf9e83d95ffb127f29e358bc019b4e32c2060
franalmeida@gmail.com <46890781+franalma@users.noreply.github.com>
2020-09-17 07:56:10 +0200

    Including new method to get logs info from file

--- src/main/java/com/diusframi/android/stats/Log.java
+++ src/main/java/com/diusframi/android/stats/Log.java
@@ -1,16 +1,40 @@
 package com.diusframi.android.stats;
 
 import android.content.Context;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class Log {
     com.diusframi.android.logs2json.Log logger;
     private static Log instance;
     private String tag = "AndroidServiceStats";
     private static Context context;
+    private final static String FILE_NAME = "android_service_stats.json";
 
     private Log(){
-        logger = new com.diusframi.android.logs2json.Log(tag, "android_service_stats.json");
+        logger = new com.diusframi.android.logs2json.Log(tag, FILE_NAME);
         logger.setToFile(true);
+    }
+
+    public String getLogFullInfo() {
+        JSONArray jac = new JSONArray();
+        try{
+            File dir = context.getFilesDir();
+            BufferedReader reader = new BufferedReader(new FileReader(dir+"/"+ FILE_NAME));
+            String line;
+            while((line = reader.readLine()) != null){
+                line = line.replaceAll("\\\\","");
+                JSONObject joc = new JSONObject(line);
+                jac.put(joc);
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jac.toString();
     }
 
     public static void init(Context c){
